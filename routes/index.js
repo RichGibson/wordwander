ead6e3980a5f979a8d938064211bb002ec8785b0
Rich Gibson <rich.gibson@gmail.com>
2015-03-30 09:25:11 +0200

    now with 'contains' query, and more good stuff

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,8 @@ router.get('/', function(req, res, next) {
 
 router.get('/prefixes', function(req, res, next) {
   root = req.query.root;
+  query = "insert into history values ('" + root + "', 'prefixes')";
+  prefixes = db.all(query, function(err,rows){ });
   //root = req.params.root;
   console.log("in index.js prefixes root:" + root);
   // prefixes = wordwander.get_roots(root);
@@ -29,9 +31,28 @@ router.get('/prefixes', function(req, res, next) {
     });
 });
 
+router.get('/contains', function(req, res, next) {
+  root = req.query.prefix;
+  query = "insert into history values ('" + root + "', 'contains')";
+  prefixes = db.all(query, function(err,rows){ });
+  console.log("in index.js contains root:" + root);
+  // prefixes = wordwander.get_roots(root);
+  query = "select word, substr(word,0,instr(word,'" + root + "')) as prefix from words where word glob '*" + root + "*'";
+
+  prefixes = "foo";
+  prefixes = db.all(query, function(err,rows){
+        console.log("in db.all query") ;
+        //console.log(rows); 
+        res.render('contains', { title: 'Contains', root: root, rows: rows });
+        //res.json({ "rows" : rows });
+    });
+});
+
 router.get('/starts', function(req, res, next) {
   console.log('this is starts');
   root = req.query.prefix;
+  query = "insert into history values ('" + root + "', 'starts')";
+  prefixes = db.all(query, function(err,rows){ });
   console.log("in index.js /starts root:" + root);
   l = root.length + 1;
   query = "select word, substr(word," + l + ") as prefix from words where word glob '" + root + "*'";
@@ -41,6 +62,16 @@ router.get('/starts', function(req, res, next) {
         console.log("in db.all query") ;
         //console.log(rows); 
         res.render('starts', { title: 'Words that start with...', root: root, prefixes: rows });
+        //res.json({ "rows" : rows });
+    });
+});
+
+router.get('/history', function(req, res, next) {
+  console.log('this is history');
+  query = "select distinct(wordvalue), query from history order by query";
+
+  history = db.all(query, function(err,rows){
+        res.render('history', { title: 'Query History', history: rows });
         //res.json({ "rows" : rows });
     });
 });
